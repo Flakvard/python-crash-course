208b9cab5b96c132523d2024325301ab92229269
flakvard <marni@falkvard.com>
2022-08-29 21:34:19 +0100

    update

--- alien.py
+++ alien.py
@@ -1,2 +1,38 @@
-alien=['color':'red','points':'5']
-print alien
+alien_0 = {'color':'red','points':5}
+print (alien_0)
+
+print(alien_0['color'])
+
+print(alien_0['points'])
+
+new_points = alien_0['points']
+print(f"You just earned {new_points} points!")
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print (alien_0)
+
+alien_1 = {}
+
+alien_1['color'] = "yellow"
+alien_1['points'] = 8
+alien_1['x_position'] = 5
+alien_1['y_position'] = 5
+
+print (alien_1)
+
+alien_0['speed'] = "medium"
+
+if alien_0['speed'] == "slow":
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    x_increment = 3
+
+alien_0['x_position'] = alien_0['x_position']+x_increment
+print(f"New position: {alien_0['x_position']}")
+
+alien_3 = {'color':'red','speed':'slow'} #'points':5 - Use get() when there is no value assigned
+point_value = alien_3.get('points','No point value assigned.')
+print (point_value)
---
+++ favorite_languages.py
@@ -0,0 +1,21 @@
+favorite_language = {
+    'Jen':'python',
+    'Sarah':'c',
+    'edward':'ruby',
+    'phil':'c++'
+}
+for name, language in favorite_language.items():
+    print(f"{name.title()}'s favorite language is {language.title()}.")
+
+for name in favorite_language.keys():
+    print(name)
+
+if 'erin' not in favorite_language.keys():
+    print("Erin, please take our poll")
+
+for name in sorted(favorite_language.keys(),reverse=True):
+    print(f"{name.title()} thank you for taking our poll.")
+
+print("The following languages have been mentioned:")
+for language in favorite_language.values():
+    print(language)
